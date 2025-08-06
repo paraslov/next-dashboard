f5725c10211500bf7275376e4802f1ff9feb0dce
Баланов Сергей Валерьевич <sergei.balanov@kaspi.kz>
2025-08-06 19:49:42 +0500

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+import { UnifrakturMaguntia } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const unifrakturMaguntia = UnifrakturMaguntia({ weight: ['400'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
